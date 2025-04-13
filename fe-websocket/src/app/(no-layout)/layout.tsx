a7a4cf51d58810ef2f582a7fbd3f8d8fb574b26e
TuanAnh <tuanmeo980provip@gmail.com>
2025-04-13 12:14:59 +0700

    fix all

--- fe-websocket/src/app/(no-layout)/layout.tsx
+++ fe-websocket/src/app/(no-layout)/layout.tsx
@@ -1,11 +1,6 @@
 import {ReactNode} from 'react';
 import "@/app/globals.css"
 import "@mantine/core/styles.css"
-import {Metadata} from "next";
-
-export const metadata: Metadata = {
-    title: 'Web Social',
-}
 
 const Layout = async ({children}: Readonly<{ children: ReactNode }>) => {
     return (
