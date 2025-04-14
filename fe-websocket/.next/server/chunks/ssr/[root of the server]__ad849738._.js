719fa206b338e4f9e4d7ca795802f7ed2cef3ec7
TuanAnh <tuanmeo980provip@gmail.com>
2025-04-15 01:24:00 +0700

    add function follow

--- fe-websocket/.next/server/chunks/ssr/[root of the server]__ad849738._.js
+++ fe-websocket/.next/server/chunks/ssr/[root of the server]__ad849738._.js
@@ -744,8 +744,8 @@ const LeftSidebar = ()=>{
         },
         {
             icon: __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$lucide$2d$react$2f$dist$2f$esm$2f$icons$2f$users$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__$3c$export__default__as__Users$3e$__["Users"],
-            label: 'Friends',
-            href: '/friends'
+            label: 'Follows',
+            href: '/follows'
         },
         {
             icon: __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$lucide$2d$react$2f$dist$2f$esm$2f$icons$2f$film$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__$3c$export__default__as__Film$3e$__["Film"],
