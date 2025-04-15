872e172529ecaa6d8598609e924042a723f51018
TuanAnh <tuanmeo980provip@gmail.com>
2025-04-15 17:29:17 +0700

    edit UI FOLLOW

--- fe-websocket/.next/static/chunks/src_app_(layout)_follows_page_tsx_f3a31083._.js
+++ fe-websocket/.next/static/chunks/src_app_(layout)_follows_page_tsx_f3a31083._.js
@@ -5,8 +5,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/src_app_(layout)_follows_page_tsx_f3a31083._.js",
   "chunks": [
-    "static/chunks/node_modules_0f4a6e4d._.js",
-    "static/chunks/src_119e13d3._.js"
+    "static/chunks/node_modules_1a5254a5._.js",
+    "static/chunks/src_a295c919._.js"
   ],
   "source": "dynamic"
 });
