9b879c7774a8a6d8caa10c33a21395c1426bb5a0
TuanBlue980 <tuanmeo980provip@gmail.com>
2025-04-30 15:08:31 +0700

    fix post card

--- fe-websocket/src/app/(layout)/messages/components/ChatSidebar.tsx
+++ fe-websocket/src/app/(layout)/messages/components/ChatSidebar.tsx
@@ -34,7 +34,7 @@ const ChatSidebar: React.FC<ChatSidebarProps> = ({
     
     if (isLoading) {
         return (
-            <div className="w-full md:w-80 bg-[#FAFAFA] dark:bg-[#121212] border-r border-[#DBDBDB] dark:border-[#262626] h-screen flex flex-col">
+            <div className="w-full md:w-80 border-r border-[#DBDBDB] dark:border-[#262626] h-screen flex flex-col">
                 <div className="p-4">
                     <div className="animate-pulse space-y-4">
                         {[...Array(5)].map((_, i) => (
